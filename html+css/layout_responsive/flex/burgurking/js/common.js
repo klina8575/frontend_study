0cbcb76fdc5667c13f67c9c5b46c84fdfdd6f907
“klina8575” <klina8575@gmail.com>
2022-02-04 17:48:56 +0900

    p

--- html+css/layout_responsive/flex/burgurking/js/common.js
+++ html+css/layout_responsive/flex/burgurking/js/common.js
@@ -1,7 +1,9 @@
-let menu = document.querySelector('.menu__main');
-let subMenu = document.querySelectorAll('.list__drop');
-let panel = document.querySelector('.header__panel');
-let header = document.querySelector('.header');
+/* 1. pc버전 서브메뉴 관련 */
+
+const menu = document.querySelector('.menu__main');
+const subMenu = document.querySelectorAll('.list__drop');
+const panel = document.querySelector('.header__panel');
+const header = document.querySelector('.header');
 
 // 메뉴에 마우스 오버시 하위메뉴 보여줌
 menu.addEventListener('mouseover', () => {
@@ -20,3 +22,70 @@ panel.addEventListener('mouseout', () => {
 	panel.style.display = 'none';
     subMenu.forEach(subMenu => { subMenu.style.display = "none" });
 });
+
+
+/* 2. 모바일 버전 메뉴 보이기, 숨기기 */
+const Mhamburger = document.querySelector('.mobile.hamburger');
+const Mclose = document.querySelector('.mobile.close');
+const Mnav = document.querySelector('.header__Mnav');
+
+
+Mhamburger.addEventListener('click', () => {
+    Mnav.style.display = 'block';
+});
+
+Mclose.addEventListener('click', () => {
+    Mnav.style.display = 'none';
+});
+
+
+/* 3. width가 767px 이상일때 모바일 메뉴 보임 방지 */
+/* 
+    과부하 방지
+    https://developer.mozilla.org/ko/docs/Web/API/Window/resize_event 
+*/
+
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+// handle event
+window.addEventListener("optimizedResize", function() {
+    let winWidth = window.innerWidth;
+
+    // 태블릿 사이즈 부터
+    if (winWidth >= 767){
+        Mnav.style.display = 'none';
+    }
+});
+
+
+/* 4. 모바일 하위 메뉴 보이기 숨기기 */
+
+// const test = document.querySelector('.test');
+// const test2 = document.querySelector('.test2');
+
+// test.addEventListener('click', () => {
+//     var tets = test.children[2].style.display;
+// });
+
+// const showHide = (e) => {
+//     if (e.children[2].style.display == 'none') {
+//         e.children[2].style.display = 'block'
+//     }
+// }
